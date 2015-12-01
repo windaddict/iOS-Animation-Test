4d6de8b6733f1236cce719d437df142d6fc7df7e
John M. P. Knox <john@windaddict.com>
2015-11-30 18:53:16 -0600

    Demonstrates weird behavior in TV target

---
+++ TV-TVAnimationRadar/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TV-TVAnimationRadar
+//
+//  Created by John M. P. Knox on 11/27/15.
+//  Copyright © 2015 John M. P. Knox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *label;
+@end
+
--- TV-TVAnimationRadar/ViewController.m
+++ TV-TVAnimationRadar/ViewController.m
@@ -10,8 +10,9 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *animatedView;
+@property (weak, nonatomic) IBOutlet UIView *uncontainedAnimatedView;
+@property (strong, nonatomic) NSTimer *timer;
 @property (assign, nonatomic) BOOL right;
-@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -37,6 +38,7 @@
  *  Animates the view from it's current position to the new position.
  */
 - (void)animateInDirection:(BOOL) right{
+    //flip the color
     UIViewAnimationOptions opts = UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveLinear | UIViewAnimationOptionOverrideInheritedDuration;
     CGRect frame = self.animatedView.frame;
     frame.origin.x = [self destinationXCoordinateInDirection:right];
@@ -63,19 +65,49 @@
     [self cancelAnimations];
     [self animateInDirection:!self.right];
     self.right = !self.right;
-//        self.label.text = self.right ? @"right" : @"left";
-//    [UIView performWithoutAnimation:^{
-//        self.containedLabel.text = self.right ? @"right" : @"left";
-//    }];
+    self.label.text = self.right ? @"right" : @"left";
 }
-
-
 
 -(CGFloat)destinationXCoordinateInDirection: (BOOL) right{
     if(right){
         return CGRectGetMaxX(self.view.bounds) - CGRectGetWidth(self.animatedView.frame);
     }
     return 0.0;
+}
+
+#pragma mark - Uncontained Reverse (broken)
+/**
+ *  Animates the uncontained view from it's current position to the new position.
+ */
+- (void)uncontainedAnimateInDirection:(BOOL) right{
+    //flip the color
+    UIViewAnimationOptions opts = UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveLinear | UIViewAnimationOptionOverrideInheritedDuration;
+    CGRect frame = self.uncontainedAnimatedView.frame;
+    frame.origin.x = [self destinationXCoordinateInDirection:right];
+    [UIView animateWithDuration:1.0 delay:0.0 options:opts animations:^{
+        self.uncontainedAnimatedView.frame = frame;
+    } completion:^(BOOL finished) {
+        if(finished){
+            //this space intentionally left blank
+        }
+    }];
+}
+
+/**
+ *  Causes any current animations on puckImage to stop, keeping the puck in the same visible location.
+ *  /return the stopping location of the puck
+ */
+-(void)cancelUncontainedAnimations{
+    self.animatedView.center = [((CALayer*)self.animatedView.layer.presentationLayer) position];
+    [self.animatedView.layer removeAllAnimations];
+    
+}
+
+- (IBAction)uncontainedReverseTapped:(id)sender {
+    [self cancelUncontainedAnimations];
+    [self uncontainedAnimateInDirection:!self.right];
+    self.right = !self.right;
+    self.label.text = self.right ? @"right" : @"left";
 }
 
 @end
